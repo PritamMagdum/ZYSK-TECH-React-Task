41b6d6cad2a5d8b4e3f42ab188c7a498cf921496
Pritam <pritammagdum24@gmail.com>
2024-11-29 17:03:47 +0530

    Updated Navbar to Navigate the Web Pages using React-Router-Dom Library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import AdvanceAnalyticsSection from "./Components/AdvanceAnalyticsSection";
+import AnalyticsSection from "./Components/AnalyticsSection";
+import FeaturesSection from "./Components/FeaturesSection";
+import FreqAskQuestions from "./Components/FreqAskQuestions";
+import Header from "./Components/Header";
+import TestimonialSection from "./Components/TestimonialSection";
 import logo from "./logo.svg";
 // import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-2xl font-medium">hello pritam</h1>
-    </div>
+    <Router>
+      <Header />
+      <AnalyticsSection />
+      <FeaturesSection />
+      <TestimonialSection />
+      <AdvanceAnalyticsSection />
+      <FreqAskQuestions />
+      <Routes>
+        {/* <Route path="/" element={} />
+        <Route path="/result" element={<Resu />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/Sheet";
+import MenuIcon from "../assets/images/menuIcon.png";
+import Logo from "../assets/images/Logo.png";
+import UserImage from "../assets/images/UserImage.png";
+import NavItems from "./NavItems";
+
+const Header = () => {
+  const [sheetOpen, setSheetOpen] = useState(false);
+  return (
+    <>
+      {/* Mobile */}
+      <nav className="md:hidden flex justify-between px-5 py-3">
+        <div className="flex justify-center items-center gap-2 text-white text-lg font-bold">
+          <img src={Logo} alt="img" className="w-[32px] h-[32px]" />
+          <span className="font-bold text-black">Untitled UI</span>
+        </div>
+        <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
+          <SheetTrigger className="align-middle">
+            <img
+              src={MenuIcon}
+              alt="menu"
+              width={24}
+              height={24}
+              className="cursor-pointer"
+            />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+            <Link
+              href="/"
+              className="w-fit"
+              onClick={() => setSheetOpen(false)}
+            >
+              <img src={Logo} alt="logo" width={50} height={50} />
+            </Link>
+            <hr className="border border-gray-50" />
+            <NavItems setOpen={setSheetOpen} />
+          </SheetContent>
+        </Sheet>
+      </nav>
+      {/* Desktop */}
+      <nav className="bg-[#FFFFFF] p-4 px-20 hidden md:block">
+        <div className="container mx-auto flex  gap-5 items-center justify-between border border-rose-500 ">
+          <div className="flex gap-4 ">
+            <div className="flex justify-center items-center gap-2 text-white text-lg font-bold">
+              <img src={Logo} alt="img" className="w-[32px] h-[32px]" />
+              <span className="font-bold text-black">Untitled UI</span>
+            </div>
+            <div className="flex gap-[15px] items-center">
+              <Link className="mx-2.5">Home</Link>
+              <Link className="mx-2.5">Products</Link>
+              <Link className="mx-2.5">Resources</Link>
+              <Link className="mx-2.5">Pricing</Link>
+            </div>
+          </div>
+          <div>
+            <img
+              src={UserImage}
+              alt="USER"
+              className="h-[40px] w-[40px] rounded-full"
+            />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
